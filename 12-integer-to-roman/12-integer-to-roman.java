0a6c9b4ae6ad7368f0c44d79a77018d13f4147aa
SURAJ DANGI <70461655+suraj1suru@users.noreply.github.com>
2022-09-16 13:06:25 +0530

    Time: 18 ms (24.23%), Space: 48.3 MB (12.30%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String intToRoman(int num) {
+//         Q. Why use LinkedHashMap instead of HashMap?
+//         Ans. LinkedHashMap is ordered, HashMap is unordered.
+//         source: https://docs.oracle.com/javase/6/docs/api/java/util/LinkedHashMap.html
+        
+        LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            while (num >= entry.getKey()) {
+                num -= entry.getKey();
+                sb.append(entry.getValue());
+            }
+        }
+        return sb.toString();
+    }
+}
