6ea28f9781f304202410e5e40bd5e01da6061c66
SURAJ DANGI <70461655+suraj1suru@users.noreply.github.com>
2022-10-13 14:51:18 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (98.88%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.java
@@ -8,7 +8,9 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
+        //so i am assigning givin node to = to next node value
         node.val  = node.next.val;
+        //and i am doing assign the address to next node to current node
         node.next = node.next.next;
     }
 }
