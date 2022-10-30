df3b55a18e4cd6b6667b97080101c826b055725a
SURAJ DANGI <70461655+suraj1suru@users.noreply.github.com>
2022-10-31 00:55:40 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (25.00%) - LeetHub

---
+++ 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    private int sum(long n) {
+        int res = 0;
+        while (n > 0) {
+            res += n % 10;
+            n /= 10;
+        }
+        return res;
+    }
+
+    public long makeIntegerBeautiful(long n, int target) {
+        long n0 = n, base = 1;
+        while (sum(n) > target) {
+            n = n / 10 + 1;
+            base *= 10;
+        }
+        return n * base - n0;
+    }
+}
